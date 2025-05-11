3cc2f754fa6b075966f994283d530935bcda5dd6
namiron <sasha_zpua@mail.ru>
2025-05-11 16:21:45 +0300

    item screen update

--- screens/item/Item.tsx
+++ screens/item/Item.tsx
@@ -8,19 +8,32 @@ const Item = () => {
   const route = useRoute<RouteProp<{ params: { laureate: ILaureateData } }, "params">>();
   const { laureate } = route.params;
 
+  const affiliationsText = laureate.affiliations?.length
+    ? laureate.affiliations.map((affiliation, index) => (
+        <Text key={index}>
+          {affiliation.name}, {affiliation.city}, {affiliation.country}
+        </Text>
+      ))
+    : null;
+
+  const year: string = "Year:";
+  const motivation: string = "Motivation:";
+  const country: string = " Country:";
+
   return (
     <ScrollView contentContainerStyle={styles.container}>
       <Text style={styles.name}>
         {laureate.firstname} {laureate.surname}
       </Text>
       <Text style={styles.info}>
-        Year: <Text style={styles.highlight}>{laureate.year}</Text>
+        {year} <Text style={styles.highlight}>{laureate.year}</Text>
       </Text>
       <Text style={styles.info}>
-        Motivation: <Text style={styles.highlight}>{laureate.motivation}</Text>
+        {motivation}
+        <Text style={styles.highlight}>{laureate.motivation}</Text>
       </Text>
       <Text style={styles.info}>
-        Category: <Text style={styles.highlight}>{laureate.category}</Text>
+        {country} <Text style={styles.highlight}>{affiliationsText}</Text>
       </Text>
     </ScrollView>
   );
