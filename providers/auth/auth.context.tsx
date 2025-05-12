3888335109901d740931adf5fdb371195a0a5f7d
namiron <sasha_zpua@mail.ru>
2025-05-12 16:47:53 +0300

    provider loading

--- providers/auth/auth.context.tsx
+++ providers/auth/auth.context.tsx
@@ -2,6 +2,7 @@ import React, { createContext, useState, useEffect } from "react";
 import * as SplashScreen from "expo-splash-screen";
 import { getUser } from "../../services/auth/auth.helper";
 import { IUser } from "../../screens/auth/types/user.types";
+import { Loading } from "../../ui/ui-loading/Loading";
 
 interface AuthContextType {
   user: IUser | null;
@@ -15,17 +16,28 @@ export const AuthContext = createContext<AuthContextType>({
 
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const [user, setUser] = useState<IUser | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
     const loadUser = async () => {
-      await SplashScreen.preventAutoHideAsync();
-      const storedUser = await getUser();
-      if (storedUser) setUser(storedUser);
-      await SplashScreen.hideAsync();
+      try {
+        await SplashScreen.preventAutoHideAsync();
+        const storedUser = await getUser();
+        if (storedUser) setUser(storedUser);
+      } catch (error) {
+        console.error((error as Error).message);
+      } finally {
+        await SplashScreen.hideAsync();
+        setLoading(false);
+      }
     };
 
     loadUser();
   }, []);
+
+  if (loading) {
+    return <Loading />;
+  }
 
   return <AuthContext.Provider value={{ user, setUser }}>{children}</AuthContext.Provider>;
 };
